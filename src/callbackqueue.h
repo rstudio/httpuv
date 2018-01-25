a2516c6f1af92bd58d6855bc0ca629defe4dcaa8
Winston Chang <winston@stdout.org>
2018-01-25 13:09:25 -0600

    Make flush method private

--- src/callbackqueue.h
+++ src/callbackqueue.h
@@ -9,9 +9,11 @@ class CallbackQueue {
 public:
   CallbackQueue(uv_loop_t* loop);
   void push(boost::function<void (void)> cb);
-  void flush();
+  // Needs to be a friend to call .flush()
+  friend void flush_callback_queue(uv_async_t *handle);
 
 private:
+  void flush();
   uv_async_t flush_handle;
   queue< boost::function<void (void)> > q;
 };
