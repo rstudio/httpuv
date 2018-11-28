2243a49cfb651630084d3a80d5785ac093a0b397
Winston Chang <winston@stdout.org>
2018-11-28 00:52:21 -0600

    Add unit tests for static file serving

---
+++ tests/testthat/test-app.R
@@ -0,0 +1,46 @@
+context("basic")
+
+test_that("Basic functionality", {
+  s1 <- startServer("127.0.0.1", random_open_port(),
+    list(
+      call = function(req) {
+        list(
+          status = 200L,
+          headers = list('Content-Type' = 'text/html'),
+          body = "server 1"
+        )
+      }
+    )
+  )
+  expect_equal(length(listServers()), 1)
+  
+  s2 <- startServer("127.0.0.1", random_open_port(),
+    list(
+      call = function(req) {
+        list(
+          status = 200L,
+          headers = list('Content-Type' = 'text/html'),
+          body = "server 2"
+        )
+      }
+    )
+  )
+  expect_equal(length(listServers()), 2)
+  
+  r1 <- fetch(local_url("/", s1$getPort()))
+  r2 <- fetch(local_url("/", s2$getPort()))
+  
+  expect_equal(r1$status_code, 200)
+  expect_equal(r2$status_code, 200)
+
+  expect_identical(rawToChar(r1$content), "server 1")
+  expect_identical(rawToChar(r2$content), "server 2")
+
+  expect_identical(parse_headers_list(r1$headers)$`content-type`, "text/html")
+  expect_identical(parse_headers_list(r1$headers)$`content-length`, "8")
+
+  s1$stop()
+  expect_equal(length(listServers()), 1)
+  stopAllServers()
+  expect_equal(length(listServers()), 0)
+})
--- tests/testthat/test-traffic.R
+++ tests/testthat/test-traffic.R
@@ -11,29 +11,6 @@ skip_if_not_possible <- function() {
   }
 }
 
-random_open_port <- function(min = 3000, max = 9000, n = 20) {
-  # Unsafe port list from shiny::runApp()
-  valid_ports <- setdiff(min:max, c(3659, 4045, 6000, 6665:6669, 6697))
-
-  # Try up to n ports
-  for (port in sample(valid_ports, n)) {
-    handle <- NULL
-
-    # Check if port is open
-    tryCatch(
-      handle <- httpuv::startServer("127.0.0.1", port, list()),
-      error = function(e) { }
-    )
-    if (!is.null(handle)) {
-      httpuv::stopServer(handle)
-      return(port)
-    }
-  }
-
-  stop("Cannot find an available port")
-}
-
-
 parse_ab_output <- function(p) {
   text <- readLines(p$get_output_file())
 
