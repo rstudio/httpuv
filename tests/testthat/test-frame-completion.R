c3fbde4505472dd2d1c1dd2f21ac631777a51245
Alan Dipert <alan@dipert.org>
2019-06-19 10:45:50 -0700

    Improve empty payload test

--- tests/testthat/test-frame-completion.R
+++ tests/testthat/test-frame-completion.R
@@ -3,31 +3,37 @@
 
 context("frame completion")
 
-elapsed <- NULL
+test_that("a close message with no payload is processed", {
+  elapsed <- NULL
 
-random_port <- random_open_port()
+  random_port <- random_open_port()
 
-srv <- startServer("127.0.0.1", random_port, list(
-  onWSOpen = function(ws) {
-    open_time <- Sys.time()
-    ws$onClose(function(e) {
-      elapsed <<- Sys.time() - open_time
-      stopServer(srv)
+  srv <- startServer("127.0.0.1", random_port, list(
+    onWSOpen = function(ws) {
+      open_time <- Sys.time()
+      ws$onClose(function(e) {
+        elapsed <<- Sys.time() - open_time
+        stopServer(srv)
+      })
+    }
+  ))
+
+  on.exit(srv$stop())
+
+  # "Unnecessary" braces here to prevent `later` from attempting to
+  # run callbacks if this test is pasted at the console
+  {
+    ws_client <- websocket::WebSocket$new(sprintf("ws://127.0.0.1:%s", random_port))
+    ws_client$onOpen(function(event) {
+      ws_client$close(NA)
     })
   }
-))
 
-# "Unnecessary" braces here to prevent `later` from attempting to
-# run callbacks if this test is pasted at the console
-{
-  ws_client <- websocket::WebSocket$new(sprintf("ws://127.0.0.1:%s", random_port))
-  ws_client$onOpen(function(event) {
-    ws_client$close(NA)
-  })
-}
+  loop_start <- Sys.time()
+  while (!later::loop_empty()) {
+    if ((Sys.time() - loop_start) > as.difftime(10, units = "secs")) stop("run loop timed out")
+    later::run_now()
+  }
 
-while (!later::loop_empty()) {
-  later::run_now()
-}
-
-testthat::expect_true(elapsed < as.difftime(0.2, units = "secs"))
+  testthat::expect_true(elapsed < as.difftime(1, units = "secs"))
+})
