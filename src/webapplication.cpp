d169d99fa2889d8285d86e691ecced551935e7e2
Lars Haferkamp <lars.haferkamp@comsysto.com>
2019-10-28 16:31:46 +0100

    Issue #247 fixed, Content-Length:0 is valid header for static files

--- src/webapplication.cpp
+++ src/webapplication.cpp
@@ -483,7 +483,8 @@ boost::shared_ptr<HttpResponse> RWebApplication::staticFileResponse(
   }
 
   // Make sure that there's no message body.
-  if (pRequest->hasHeader("Content-Length") || pRequest->hasHeader("Transfer-Encoding")) {
+  if ((pRequest->hasHeader("Content-Length") && pRequest->getHeader("Content-Length") != "0")
+        || pRequest->hasHeader("Transfer-Encoding")) {
     return error_response(pRequest, 400);
   }
 
